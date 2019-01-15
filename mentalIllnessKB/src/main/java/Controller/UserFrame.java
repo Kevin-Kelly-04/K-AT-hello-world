b7a09bbb06866168d28db59f3aac06bbf017b7c8
Thomas Bakker <Thomas@AEX.local>
2019-01-15 15:00:46 +0100

    Working version: clicking buttons etc

--- mentalIllnessKB/src/main/java/Controller/UserFrame.java
+++ mentalIllnessKB/src/main/java/Controller/UserFrame.java
@@ -1,6 +1,8 @@
 package Controller;
 import View.*;
 import Model.*;
+import javafx.scene.control.SplitPane;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
@@ -9,82 +11,62 @@ import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 
 public class UserFrame extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
-	private UserPanel userPanel = new UserPanel();
+	private int index = 0;
+	//private UserPanel userPanel = new UserPanel();
 	private UserModel userModel = new UserModel();
+	private JSplitPane splitPane = new JSplitPane();
 	private JFrame userFrame = new JFrame("Mental Illness");
-	public JPanel basePanel = new JPanel();
+	public JPanel questionPanel = new JPanel();
+    public JPanel answerPanel = new JPanel();
+    public JLabel questionLabel = new JLabel("Default label");
 
     public UserFrame(UserModel model) {
         this.setUserModel(model);
-        userFrame.setSize(1400, 800);
+        userFrame.setSize(600, 300);
         userFrame.setDefaultCloseOperation(3);
-        //basePanel.setLayout(new BoxLayout(basePanel, BoxLayout.Y_AXIS));
-        //basePanel.setBorder(new EmptyBorder(new Insets(getWidth()/2, getHeight()/2+50, 3, 3)));
-        basePanel.setBackground(new Color(222, 212, 169));
-        basePanel.setLocation(getWidth()/2, getHeight() /20);
-        /*JPanel header = new JPanel();
-        frame.add(header, "North");
-        header.setLayout(new FlowLayout(1, 5, 5));
-        header.setBackground(new Color(222, 212, 169));*/
-        this.userPanel.setUserFrame(this);
-        userFrame.add(this.userPanel);
-        //userFrame.add(basePanel);
+        userFrame.add(splitPane, BorderLayout.CENTER);
+
+        splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+        splitPane.setDividerLocation(150);
+        splitPane.setTopComponent(questionPanel);
+        splitPane.setBottomComponent(answerPanel);
+
+        questionPanel.add(questionLabel);
+
+        answerPanel.setLayout(new BoxLayout(answerPanel, BoxLayout.PAGE_AXIS));
+        answerPanel.add(new JButton("Button"));
+        JButton newButton = new JButton("Hello");
+        newButton.addActionListener(this);
+        answerPanel.add(newButton);
+        answerPanel.add(new JButton("Button two"));
+
         userFrame.setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e) {
-        /*if (e.getActionCommand().equals("newGraphModel")) {
-            this.setGraphModel(new GraphModel());
-        } else if (e.getActionCommand().equals("saveGraphModel")) {
-            this.saveGraphModel();
-        } else {
-            if (e.getActionCommand().equals("openGraphModel")) {
-                GraphModel graphModel = this.loadGraphModelDialog();
-                if (graphModel != null) {
-                    this.setGraphModel(graphModel);
-                }
-            }
-
-        }*/
+        questionLabel.setText((String)userModel.getQuestions().get(index));
+        answerPanel.removeAll();
+        ArrayList listAnswers = (ArrayList) userModel.getAnswers().get(index);
+        for (int idx = 0; idx < listAnswers.size(); idx++ ) {
+            JButton newButton = new JButton((String)listAnswers.get(idx));
+            newButton.addActionListener(this);
+            answerPanel.add(newButton);
+        }
+        splitPane.setBottomComponent(answerPanel);
+        splitPane.setDividerLocation(150);
+        userFrame.repaint();
+        userFrame.setVisible(true);
+        index++;
     }
 
     public void setUserModel(UserModel model) {
         this.userModel = model;
-        this.userPanel.setModel(model);
-        model.addObserver(this.userPanel);
     }
-
-	public void initialize() {
-		JFrame frame = new JFrame("Mental illness - Knowledge Technology Practical");
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setSize(900,1200);
-	    
-	    /*JPanel basePanel = new JPanel();
-
-        basePanel.setLayout(new BoxLayout(basePanel, BoxLayout.Y_AXIS));
-        basePanel.setBorder(new EmptyBorder(new Insets(frame.getWidth()/2, frame.getHeight()/2+50, 3, 3)));
-
-        basePanel.add(new JButton("Yes"));
-        basePanel.add(Box.createRigidArea(new Dimension(0, 5)));
-        /*basePanel.add(new JButton("No"));
-        basePanel.add(Box.createRigidArea(new Dimension(0, 5)));
-        basePanel.add(new JButton("Maybe"));
-        basePanel.add(Box.createRigidArea(new Dimension(0, 5)));
-        basePanel.add(new JButton("..."));
-        //basePanel.setLocation(frame.getWidth(), frame.getHeight());
-        basePanel.setLocation(450, 600);
-        frame.add(basePanel);*/
-        /*FrameDragListener frameDragListener = new FrameDragListener(frame);
-        frame.addMouseListener(frameDragListener);
-        frame.addMouseMotionListener(frameDragListener);*/
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-	}
 
 	
 }
